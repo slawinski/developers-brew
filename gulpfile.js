c2d4222594c10d0b06047656b61b47e5f9a2f803
slawinski <piotr.slawinski1@gmail.com>
2018-04-30 16:01:59 +0200

    Commented some stuff in the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const gulp = require('gulp'),
       maps = require('gulp-sourcemaps'),
        del = require('del');
 
+//This task joins many script files into one
 gulp.task('concatScripts', function () {
   return gulp.src('src/js/*.js')
     .pipe(maps.init())
@@ -14,6 +15,7 @@ gulp.task('concatScripts', function () {
     .pipe(gulp.dest('src/js'))
 });
 
+//This task removes whitespaces, newlines etc. from code to decrease its size
 gulp.task('minifyScripts', ['concatScripts'], function(){
   return gulp.src('src/js/app.js')
   .pipe(uglify())
@@ -21,6 +23,7 @@ gulp.task('minifyScripts', ['concatScripts'], function(){
   .pipe(gulp.dest('src/js'))
 })
 
+//This task compiles Sass files into CSS
 gulp.task('compileSass', function () {
   return gulp.src('src/sass/*.scss')
     .pipe(maps.init())
@@ -29,22 +32,28 @@ gulp.task('compileSass', function () {
     .pipe(gulp.dest('src/css'));
 });
 
+//This task watches for changes in the given filetypes
 gulp.task('watchFiles', function(){
   gulp.watch('sass/**/*.scss',['compileSass']);
   gulp.watch('js/*.js', ['concatScripts']);
 })
 
+//This task deletes built files (usually before the next buid task)
 gulp.task('clean', function(){
   del(['dist', 'src/css', 'src/js/app*.js*']);
 })
 
+//This task builds the dist files in the given order
 gulp.task('build', ['minifyScripts', 'compileSass'], function(){
-  return gulp.src(['src/css/style.css', 'src/js/app.min.js', 'src/img/**', 'src/fonts/**'], {base: './'})
+  return gulp.src(['src/css/style.css', 'src/js/app.min.js', 'src/img/**', 'src/fonts/**'], {base: './src'})
   .pipe(gulp.dest('dist'));
 });
 
+//I don't remember what this does lol
 gulp.task('serve', ['watchFiles']);
 
+
+//This default task shoudl be run with "gulp" command and all should happen automagically
 gulp.task('default', ['clean'], function(){
   gulp.start('build');
 });
