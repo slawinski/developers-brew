7e59a49c9c698443d3b77b780f838a15abbc6041
slawinski <piotr.slawinski1@gmail.com>
2018-05-01 00:02:29 +0200

    Change on Karolina's request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Developer\'s Brew' });
+  res.render('index', { title: 'Developer\'s Brew LOL' });
 });
 
 module.exports = router;
