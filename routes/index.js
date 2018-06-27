dbad50b507a94b0762f80d269157338bd7e44a19
slawinski <piotr.slawinski1@gmail.com>
2018-06-27 23:59:05 +0200

    Improved styling

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
 
-router.get('/brewdog', function (req, res) {
-  console.log('Hi, I\'m rendering new layout');
-  res.render('brewDogData', {
-    title: 'Brewdog API',
-    layout: 'brewdog-layout.hbs'
-  });
-});
+// router.get('/brewdog', function (req, res) {
+//   console.log('Hi, I\'m rendering new layout');
+//   res.render('brewDogData', {
+//     title: 'Brewdog API',
+//     layout: 'brewdog-layout.hbs'
+//   });
+// });
 
-router.get('/search', function (req, res) {
+router.get('/brewdog', function (req, res) {
   var url = ('https://api.punkapi.com/v2/beers?ids=' + req.query.ids);
   console.log('>>>>>>>>>>>>>> Hi, I\'m fetching,', url, 'and rendering new layout')
   fetch(url)
